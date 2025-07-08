32cbea76f7ec7b23647c0ff80babb8d55018e9bd
Lula Robertson <lrobertson4@luc.edu>
2025-07-08 23:10:30 +0000

    needs testing

--- week06.py
+++ week06.py
@@ -1,14 +1,13 @@
 def load_to_list(filepath: str) -> list[float]:
     with open(filepath, "r") as file:
-        lines = file.readlines()
-    return [float(line.strip()) for line in lines if line.strip()] #write numbers as floats, without \n
-        
-temps = load_to_list("data/temperatures.txt") #importing the text
-print(temps)
+        temps = [float(line.strip()) for line in file if line.strip()]
+        print(temps)
+
+load_to_list("data/temperatures.txt")
 
 def descriptive_statistics(source_data: list[float]) -> None:
     with open(source_data, "r") as file:
-        temps = [float(line.strip()) for line in file if line.strip()]
+        temps = [float(line.strip()) for line in file if line.strip()] #.strip() - filtering out empty spaces.
     num_temps = len(temps)
     ave = sum(temps) / num_temps
     highest = max(temps)
@@ -16,6 +15,22 @@ def descriptive_statistics(source_data: list[float]) -> None:
     print(f"\nThere are {num_temps} values in the data source.\nThe average value is {ave:.2f}.\nThe highest value is {highest} and the smallest value is {lowest}.")
 
 descriptive_statistics("data/temperatures.txt")
+
+def apply_markup(filepath: str) -> None:
+    with open(filepath, "r") as file:
+        for line in file:
+            words = line.strip().split() # words do not contain spaces, tabs, etc. and are split with spaces
+            formatted = []
+            for word in words:
+                if word.startswith("."):
+                    formatted.append(word[1:].upper()) # adding new item to the end of list, and slicing the first character
+                elif word.startswith("_"):
+                    formatted.append(' '.join(word[1:])) #adding spaces between letters
+                else:
+                    formatted.append(word) # leave other words alone.
+            print(' '.join(formatted))
+
+apply_markup("data/markup.txt")
 
 
 
