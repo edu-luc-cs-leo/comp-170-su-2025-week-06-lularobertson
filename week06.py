548b7d2586f63d2641ecbd04cec28df7277b8a2c
Lula Robertson <lrobertson4@luc.edu>
2025-07-09 22:59:27 +0000

    final commit

--- week06.py
+++ week06.py
@@ -32,7 +32,39 @@ def apply_markup(filepath: str) -> None:
 
 apply_markup("data/markup.txt")
 
+"""
+REFLECTION:
+intersection:
+the code for my intersection was a lot shorter than the one you wrote.
+I made my result be empty, and then used an if statement inside of a loop for elements in foo
+stating that if it was in bar and not result (so the list wasn't empty), then the result would
+be the empty result + whatever the intersection was. It passed the tests.
 
+alphabetical:
+My code involved ascii values. So if the values were not corresponding to a letter, then
+the result was false, or if the i-1 ascii was greater than the i ascii the result would be
+false too.
+
+letters:
+This one also involved ascii values where they had to be in a certain range, the program
+would return the result (which was the transformed string) and if was empty, it would
+return None.
+
+generate palindrome:
+Here, I made a variable reversed_string like in your code "pali", and used a while loop
+to count i down from len(string). Then made result = reversed_string + string[i].
+I think this one is a lot more similar to your code than the other ones.
+
+is palindrome:
+Our code here is also similar, but I again used ascii values to filter out non-letter
+characters.
+
+- I think my code works well. All of it passed the tests and I feel comfortable with the
+way I did things.
+
+
+
+"""
 
 
 #--------------------------------------------------------------------------------#
